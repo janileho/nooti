b62d4a55521464d85e5dfc98dbb68283b74e4e42
janileho <jani.jl.lehto@gmail.com>
2025-08-09 19:23:37 +0300

    chore: add rewrite for /telegram-webhook/:secret to /api/telegram

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "cdn.pixabay.com" },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/telegram-webhook/:secret",
+        destination: "/api/telegram?secret=:secret",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
